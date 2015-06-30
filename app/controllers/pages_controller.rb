02271997df95954adedf1965e89595a5199b51d9
guillermo-salazar <guillermos_2007@yahoo.com>
2015-06-29 23:53:18 -0500

    added events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @dish = current_user.dishes.build if logged_in?
+    @event = current_user.events.build if logged_in?
   end
 
   def help
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
     @dishes = @user.dishes.paginate(page: params[:page])
+    @events = @user.events.paginate(page: params[:page])
   end
 
   def new
