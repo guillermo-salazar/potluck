1d26d29a8b5ee017c5bc873f1af1e7c1ff246c2a
guillermo-salazar <guillermos_2007@yahoo.com>
2015-08-17 13:47:00 -0500

    Updated to events controller and homepage layout to differentiate between owned events and events going

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,9 @@ class PagesController < ApplicationController
     if logged_in?
       @user = current_user
       @dish = current_user.dishes.build
-      @event = current_user.events.build
+      @event = current_user.owned_events.build
       @dishes = current_user.dishes.paginate(page: params[:page])
-      @events = current_user.owned_events.paginate(page: params[:page])
+      @owned_events = current_user.owned_events.paginate(page: params[:page])
       @invitations = current_user.invitations.where(accept: nil).paginate(page: params[:page])
       @sent_invitations = current_user.sent_invitations.paginate(page: params[:page])
       # @users = User.all
